1061857afc13fd517949d1d84081b02884180ca6
Aunz <thegunman555@gmail.com>
2018-04-20 15:55:54 +0700

    Update Homework

---
+++ main.js
@@ -0,0 +1,28 @@
+var Coin = /** @class */ (function () {
+    function Coin(coin) {
+        this.id = coin.id;
+        this.symbol = coin.symbol;
+        this.price_thb = coin.price_thb;
+    }
+    Coin.prototype.print = function () {
+        return " <a href=\"https://coinmarketcap.com/currencies/" + this.id + "/\"> " + this.symbol + "</a>  " + this.price_thb + " \u0E1A\u0E32\u0E17 ";
+    };
+    return Coin;
+}());
+function getcoins() {
+    return fetch("https://api.coinmarketcap.com/v1/ticker/?convert=THB&limit=20").then(function (resp) {
+        return resp.json();
+    });
+}
+function addCoin(coin) {
+    var $coins = $('#coins');
+    $coins.append(("<li> " + coin.print() + " <li>"));
+}
+function displayCoins(coins) {
+    coins.forEach(function (coin) {
+        addCoin(new Coin(coin));
+    });
+}
+getcoins().then(function (coins) {
+    displayCoins(coins);
+});
---
+++ main.ts
@@ -0,0 +1,63 @@
+declare var $: any;
+
+
+class Coin {
+    id : string
+	symbol: string
+	price_thb: number
+	
+
+	constructor(coin : {id:string,price_thb:number,symbol:string}) {
+		this.id = coin.id
+		this.symbol = coin.symbol
+		this.price_thb = coin.price_thb
+
+	}
+
+	print(): string {
+		return ` <a href="https://coinmarketcap.com/currencies/${this.id}/"> ${this.symbol}</a>  ${this.price_thb} บาท `
+
+	}
+
+
+}
+
+
+
+function getcoins() {
+
+	return fetch("https://api.coinmarketcap.com/v1/ticker/?convert=THB&limit=20").then(function (resp) {
+		return resp.json();
+
+
+
+
+	})
+}
+
+
+
+function addCoin(coin: Coin): void {
+	const $coins = $('#coins')
+	$coins.append((`<li> ${coin.print()} <\li>`)
+
+	)
+}
+
+function displayCoins(coins: Coin[]): void {
+	coins.forEach(function (coin) {
+		addCoin(new Coin(coin))
+
+
+
+	})
+}
+
+
+getcoins().then((coins) => {
+	displayCoins(coins)
+
+
+
+})
+
